a18d1427f8a3178a1b79b0df4b95323d619d7540
Roman <grv1510@mail.ru>
2025-11-14 00:39:25 +1000

    developed new logic

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import logic
+
 print('Start')
-a = 10
-b = 20
-c = 30
-d = 40
+k = 100
+a = 1 * k
+b = 2 * k
+c = 3 * k
+d = 4 * k
 print(a, b, c, d, sep = '\n')
 
 for i in range(5, 10):
-    print(i * 10)
+    print(i * k)
+    
+print(logic.rule1 + logic.rule2 + logic.rule3)
 print('end')
